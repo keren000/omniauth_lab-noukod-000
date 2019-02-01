27630c18a59f0b906f848f4bfeba217d156eb3e8
Keren-Happuc Fleurimond <keren@noukod.com>
2019-02-01 17:16:18 +0000

    Automatically backed up by Learn

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,9 +3,9 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_by(uid: auth["uid"]) do |user|
       user.name = auth["info"]["name"]
-    end 
+    end
     session[:user_id] = user.try(:id)
-  end 
+  end
 
   def auth
     request.env['omniauth.auth']
